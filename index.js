a0d54313da7c20767bfc6447755237991fe6c327
Kharel Pawan <98215488+kharelpawan@users.noreply.github.com>
2023-12-08 17:09:05 +0545

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const { default: mongoose } = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
-app.get('/api/test', (req,res)=>{
-    res.json('test ok')
-})
+
 app.post('/api/transaction', async(req,res)=>{
     // console.log(process.env.MONGO_URL)
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect('mongodb+srv://kharelpawannp:hktH4d3pJ9JxI35f@cluster0.g2ggnyb.mongodb.net/?retryWrites=true&w=majority');
     const {price, name, description, datetime}= req.body;
     const transaction = await Transaction.create({price, name,description,datetime})
     res.json(transaction)
